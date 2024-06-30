67f7af7c1c96a8b60eb977734e347ad45decac52
Nimetullah Necmettin <91189127+nimmet@users.noreply.github.com>
2024-06-30 12:16:16 +0200

    create component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template:`<p>Hello World!</p>`,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  // styles: [],
+  
 })
 export class AppComponent {
-  title = 'basics';
+  name = "Uyghur";
+  imgUrl = "https://picsum.photos/id/237/500/500";
+
+  getName() { return this.name; }
+  changeImage(e:KeyboardEvent){
+    this.imgUrl = (e.target as HTMLInputElement).value;
+  }
+
+
+  changeName(e:KeyboardEvent){
+    this.name = (e.target as HTMLInputElement).value;
+  }
+
 }
